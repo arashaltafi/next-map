ab5a8680264343cd2c65bffc8bca39f7879dd47b
Arash Altafi <arashaltafi1377@gmail.com>
2025-05-30 21:09:34 +0430

    a

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,8 +1,18 @@
+"use client"
+
 import React from 'react'
+import World from "@react-map/world";
+import Iran from "@react-map/iran";
 
 const MapPage = () => {
     return (
-        <div>MapPage</div>
+        <div className='w-full min-h-screen items-center justify-around flex flex-col px-16 py-8 bg-rose-100 overflow-x-hidden'>
+            <h1 className='text-7xl text-rose-800 font-bold'>Map Sample</h1>
+
+            <World onSelect={(state: string | null, selectedStates?: string[]) => alert(state)} size={800} hoverColor="orange" type='select-single' />
+
+            <Iran onSelect={(state: string | null, selectedStates?: string[]) => alert(state)} size={800} hoverColor="orange" type='select-single' />
+        </div>
     )
 }
 
